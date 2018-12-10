dac6ab65ae893a041f6cd3ef4b8266703fff3c99
heniolah <32879482+heniolah@users.noreply.github.com>
2018-12-10 18:03:34 +0200

    Add files via upload

---
+++ criptare_hash/criptare_hash/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace criptare_hash
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public TextReader text;
+        public string[] elements = null;
+        public byte[] key;
+        public int count;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            text = new StreamReader(@"..\..\mesaj.txt");
+            using (OpenFileDialog ofd = new OpenFileDialog() { Multiselect = false, ValidateNames = true, Filter = "FILE|*.txt" })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    if (ofd.FileName == text.ToString())
+                        textBox1.Text = (ofd.FileName);
+                    else MessageBox.Show("You didn't open the right file");
+                }
+            }
+           
+            string buffer; count = 0;
+            elements = new string[9];
+            while ((buffer = text.ReadLine()) != null)
+            {
+                string[] s = buffer.Split(' ');
+                listBox1.Items.Add(buffer);
+                elements[count] = buffer;
+                count++;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            key = new byte[16];
+            string selectedItem1 = comboBox1.Items[comboBox1.SelectedIndex].ToString();
+            string selectedItem2 = comboBox2.Items[comboBox2.SelectedIndex].ToString();
+            listBox1.Items.Add(" ");
+            if (selectedItem1 == "SHA1" && selectedItem2 == "HexHash")
+                for (int i = 0; i < count; i++)
+                    listBox1.Items.Add(SimpleHash.HexHash(SHA1.Create(), elements[i].ToString()));
+            else if (selectedItem1 == "SHA1" && selectedItem2 == "Hash")
+                for (int i = 0; i < count; i++)
+                    listBox1.Items.Add(Convert.ToBase64String(SimpleHash.Hash(SHA1.Create(), key)));
+            else if (selectedItem1 == "SHA1" && selectedItem2 == "Base64Hash")
+                for (int i = 0; i < count; i++)
+                    listBox1.Items.Add(SimpleHash.Base64Hash(SHA1.Create(), elements[i].ToString()));
+            else if (selectedItem1 == "SHA256" && selectedItem2 == "HexHash")
+                for (int i = 0; i < count; i++)
+                    listBox1.Items.Add((SimpleHash.HexHash(SHA256.Create(), elements[i].ToString())));
+            else if (selectedItem1 == "SHA256" && selectedItem2 == "Hash")
+                for (int i = 0; i < count; i++)
+                    listBox1.Items.Add(Convert.ToBase64String(SimpleHash.Hash(SHA256.Create(), key)));
+            else if (selectedItem1 == "SHA256" && selectedItem2 == "Base64Hash")
+                for (int i = 0; i < count; i++)
+                    listBox1.Items.Add((SimpleHash.Base64Hash(SHA256.Create(), elements[i].ToString())));
+            else if (selectedItem1 == "MD5" && selectedItem2 == "HexHash")
+                for (int i = 0; i < count; i++)
+                    listBox1.Items.Add((SimpleHash.HexHash(MD5.Create(), elements[i].ToString())));
+            else if (selectedItem1 == "MD5" && selectedItem2 == "Hash")
+                for (int i = 0; i < count; i++)
+                    listBox1.Items.Add(Convert.ToBase64String(SimpleHash.Hash(MD5.Create(), key)));
+            else if (selectedItem1 == "MD5" && selectedItem2 == "Base64Hash")
+                for (int i = 0; i < count; i++)
+                    listBox1.Items.Add((SimpleHash.Base64Hash(MD5.Create(), elements[i].ToString())));
+        }
+    }
+}
---
+++ criptare_hash/criptare_hash/HashAlgorithm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace criptare_hash
+{
+    public class SimpleHash
+    {
+        
+        public static byte[] Hash(HashAlgorithm algorithm, byte[] input)
+        {
+            return algorithm.ComputeHash(input);
+        }
+
+        public static string HexHash(HashAlgorithm algorithm, string text)
+        {
+            byte[] input = UnicodeEncoding.Unicode.GetBytes(text);
+            return string.Concat(Hash(algorithm, input).Select(b => b.ToString("x2")));
+        }
+
+        public static string Base64Hash(HashAlgorithm algorithm, string text)
+        {
+            byte[] input = UnicodeEncoding.Unicode.GetBytes(text);
+            return Convert.ToBase64String(Hash(algorithm, input));
+        }
+    }
+
+     //algoritmul de mai jos este doar o schita de incercare de implementare a algoritmului hash
+
+    //public abstract class HashAlgorithm : SHA1CryptoServiceProvider
+    //{
+    //    
+    //    public void Dispose()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public int TransformBlock(byte[] inputBuffer, int inputOffset, int inputCount, byte[] outputBuffer, int outputOffset)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public byte[] TransformFinalBlock(byte[] inputBuffer, int inputOffset, int inputCount)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+}
