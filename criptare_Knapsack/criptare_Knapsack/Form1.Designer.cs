9359d5d3152adfea84137d201711dad41d1a8382
heniolah <32879482+heniolah@users.noreply.github.com>
2019-01-14 11:41:49 +0200

    Add files via upload

---
+++ criptare_Knapsack/criptare_Knapsack/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace criptare_Knapsack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(489, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(489, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 48);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(163, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(205, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(163, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(205, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(163, 128);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(205, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "private key:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(131, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "n:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(80, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "m (Modulus):";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(29, 203);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(876, 251);
+            this.listBox1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 485);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ criptare_Knapsack/criptare_Knapsack/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace criptare_Knapsack
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public TextReader text;
+        public string[] elements = null;
+        public byte[] key;
+        public int count;
+        public byte[][] encryptedData;
+        public byte[][] decryptedData;
+
+        public UnicodeEncoding ByteConverter = new UnicodeEncoding();
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "1,9,4,10,30,40";
+            textBox2.Text = "53";
+            textBox3.Text = "120";
+        }
+
+        public string getcipher(string publickey, string data)
+        {
+            string data_result = "";
+            string[] vals = publickey.Split(',');
+            int[] weights = new int[vals.Length];
+
+            for (int i = 0; i < vals.Length; i++)
+             weights[i] = Convert.ToInt32(vals[i]);
+
+
+            int ptr = 0;
+            int bit = 0;
+            int total = 0;
+            do
+            {
+                total = 0;
+                for (int i = 0; i < vals.Length; i++)
+                {
+                    if (data[ptr] == '1') bit = 1;
+                    else bit = 0;
+                    total += weights[i] * bit;
+                    ptr++;
+                }
+                if (data_result == "") data_result += total.ToString();
+                else  data_result += "," + total.ToString();
+                    
+            } while (ptr < data.Length);
+
+
+            return (data_result);
+
+        }
+        public string getknap(string key, string n, string m)
+        {
+            string[] vals = key.Split(',');
+            string k = "";
+            foreach (string v in vals)
+            {
+                int i = (Convert.ToInt32(v) * Convert.ToInt32(n)) % Convert.ToInt32(m);
+                if (k == "") k += i.ToString();
+                else k += "," + i.ToString();
+
+            }
+            return (k);
+
+        }
+        //calculates & returns the inverse of a modulo n
+        public int modInverse(int a, int n)
+        {
+            int i = n, v = 0, d = 1;
+            while (a > 0)
+            {
+                int t = i / a, x = a;
+                a = i % x;
+                i = x;
+                x = d;
+                d = v - t * x;
+                v = x;
+            }
+            v %= n;
+            if (v < 0) v = (v + n) % n;
+            return v;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            text = new StreamReader(@"..\..\mesaj.txt");
+            using (OpenFileDialog ofd = new OpenFileDialog() { Multiselect = false, ValidateNames = true, Filter = "FILE|*.txt" })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    if (ofd.FileName == text.ToString())
+                        textBox1.Text = (ofd.FileName);
+
+                }
+                // else MessageBox.Show("You didn't open the right file");
+            }
+
+            string buffer; count = 0;
+            elements = new string[9];
+            while ((buffer = text.ReadLine()) != null)
+            {
+                string[] s = buffer.Split(' ');
+                listBox1.Items.Add(buffer);
+                elements[count] = buffer;
+                count++;
+            }
+
+           
+            listBox1.Items.Add(" ");
+            string priv_key = textBox1.Text;
+            string n = textBox2.Text;
+            string m = textBox3.Text;
+            string get_public = getknap(priv_key, n, m);
+            string data = "",cipher="", plain="";
+            int modinv=0;
+
+            for (int i = 0; i < count; i++)
+            {
+                 foreach (char ch in elements[i])
+                   {
+                      data += Convert.ToString((int)ch, 2); 
+                   }
+            }
+            
+            cipher = getcipher(get_public, data.ToString());
+            modinv = modInverse(Convert.ToInt32(n), Convert.ToInt32(m));
+            plain = getknap(cipher, Convert.ToString(modinv), m);
+
+            listBox1.Items.Add("datatext_bytes:" +data);
+            listBox1.Items.Add("public key: " + get_public);
+            listBox1.Items.Add("cipher: " + cipher);
+            listBox1.Items.Add("plain text: " + plain);
+            listBox1.Items.Add("inverse: " + modinv.ToString());
+            listBox1.Items.Add(" ");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(" ");
+            string data = "";
+            decryptedData = new byte[listBox1.Items.Count][];
+
+            for (int i = 0; i < count; i++)
+            {
+                foreach (char ch in elements[i])
+                {
+                    data += Convert.ToString((int)ch, 2);
+                }
+            }
+              for (int i = 0; i < count; i++)
+            decryptedData[i] = ByteConverter.GetBytes(data);
+
+            listBox1.Items.Add("Textul decriptat (acelasi cu cel original):");
+            listBox1.Items.Add(ByteConverter.GetString(decryptedData[0])).ToString();
+        }
+    }
+}
